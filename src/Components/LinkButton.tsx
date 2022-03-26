778819011f87b16ea4a60662b62f68d4ee381ef6
casslabath <casslabath@gmail.com>
2022-03-26 18:03:44 -0500

    move svg to component and style button

--- src/Components/LinkButton.tsx
+++ src/Components/LinkButton.tsx
@@ -5,12 +5,24 @@ import { NavigationContext } from '../Contexts/NavigationContext';
 
 const StyledButton = styled.button`
     background-color: #4F5D73;
-    border-radius: 32px;
+    border-radius: 16px;
+    border-style: none;
     color: #ffffff;
     font-family: "Roboto", sans-serif;
-    font-size: 24px;
-    font-weight: bold;
-    padding: 19px 27px;
+    font-size: 16px;
+    padding: 16px 24px;
+
+    &:hover {
+        cursor: pointer;
+        font-size: 18px;
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+    }
+
+    &:active {
+        cursor: pointer;
+        font-size: 18px;
+        box-shadow: none;
+    }
 `;
 
 type LinkButtonProps = {
